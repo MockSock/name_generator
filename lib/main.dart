737c7f8d9becd3d59b4ba092d9c216f546a9a37d
madden20202 <business.mocksock@gmail.com>
2022-08-03 18:34:59 -0500

    move random words over to its own stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,19 +9,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // set variables here to not have to put them in constructor
-    // creates random word pair variable
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
-          // pascal case is just every word capitalized
-          child: Text(wordPair.asPascalCase),
-        ),
+        body: const Center(
+            // will move random to another file
+            ),
       ),
     );
   }
